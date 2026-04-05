1c7ccd7bfb1506058d1e471b087c97a0c91ca9e6
LeoSergio <leandrooos222@gmail.com>
2026-04-05 02:13:37 -0300

    new pages

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
 function toggleMode() {
-  /*variavel*/
-  const html = document.documentElement
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-  html.classList.toggle("light")
-
-  //pegar a tag img no html
-  const img = document.querySelector("#profile img")
-
-  //substituir a imagem
+  const img = document.querySelector("#avatar-img");
   if (html.classList.contains("light")) {
-    //se tiver light mode add a imagem light
-    img.setAttribute("src", "assets/assets/perfil-light.png")
+    img.setAttribute("src", "assets/assets/perfil-light.png");
+    img.setAttribute("alt", "Foto de perfil - modo claro");
   } else {
-    //se tiver sem o light mode, manter a foto normal
-    img.setAttribute("src", "assets/assets/perfil-dark.png")
+    img.setAttribute("src", "assets/assets/perfil-dark.png");
+    img.setAttribute("alt", "Foto de perfil - modo escuro");
   }
-  /*oque acontceu aí em cima? Mudança de imagem , quando tiver no light mode ser uma foto e quando tiver no dark mode outra foto
-setAttribute=Modificar atributo ou personalizar*/
-
-//mudar o text alt de cada imagem
-const alt = document.documentElement
-
-  if(html.classList.contains('light')) {
-//se contem um alt no modo light detalhar o texto
-alt.setAttribute('alt','Prof de oculos com o fundo escuro')
-}else {
-//se nao contem no modo light manter texto
-alt.setAttribute('alt','Prof de oculos com o fundo amarelo')
-
 }
-}
-
-
-
-
-
-
-
