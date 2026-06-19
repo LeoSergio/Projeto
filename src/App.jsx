8c4179780de847629d3fecc7028a753858cb10d2
LeoSergio <leandrooos222@gmail.com>
2026-06-19 10:09:06 -0300

    chore: estrutura base React

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Curriculo from './pages/Curriculo'
+import Portfolio from './pages/Portfolio'
+import SobreMim from './pages/SobreMim'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/curriculo" element={<Curriculo />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/sobre" element={<SobreMim />} />
+    </Routes>
+  )
+}
